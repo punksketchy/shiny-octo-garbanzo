3f2c6a064dad062a56c189ba71c5e780ec5a85d7
Hardegree <217366@NTTDATA.COM>
2021-12-21 10:09:24 -0600

    cleanup

--- FindColumn.ps1
+++ FindColumn.ps1
@@ -6,6 +6,6 @@ $listTitle = Get-PnPList
 foreach($list in $lists) {
     $list.Fields | Select-Object Title, InternalName, @{
         label='List Title'
-        expression={$listTitle | Select-Object Title where $list.Field = $list.Field}
+        expression={$listTitle | Select-Object Title}
     }
 }
