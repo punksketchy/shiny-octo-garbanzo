788b96c507bab833315b3cf5cff3c791bcfb1f48
Scott Hardegree <scott.hardegree@nttdata.com>
2025-07-23 15:35:05 -0500

    A Script For Removing a User from All Sites in a site collection

---
+++ RemoveUserFromAllSites.ps1
@@ -0,0 +1,25 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$SiteCollectionUrl,
+
+    [Parameter(Mandatory = $true)]
+    [string]$UserLogin
+)
+
+# Connect to the site collection
+Connect-PnPOnline -Url $SiteCollectionUrl -Interactive
+
+# Get all webs (including root web)
+$webs = Get-PnPSubWeb -Recurse
+$webs += Get-PnPWeb
+
+foreach ($web in $webs) {
+    Write-Host "Processing site: $($web.Url)"
+    try {
+        # Remove user from the web
+        Remove-PnPUser -LoginName $UserLogin -Web $web -ErrorAction Stop
+        Write-Host "Removed $UserLogin from $($web.Url)"
+    } catch {
+        Write-Warning "Could not remove $UserLogin from $($web.Url): $_"
+    }
+}
